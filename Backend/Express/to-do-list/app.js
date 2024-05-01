370c4abe8e95621face28995dbaf36453ed63df2
Valderlan Silva <valderlanjosr15@gmail.com>
2024-05-01 13:06:05 -0300

    conteúdo sobre EJS

--- Backend/Express/to-do-list/app.js
+++ Backend/Express/to-do-list/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+
 const checklistRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
 require('./config/database');
+
 
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
+
 /*
 // middleware para verificar se quando fez a chamada web existe algum json disponivel
 app.use(express.json());
