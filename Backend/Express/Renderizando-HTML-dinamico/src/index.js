e17e11a2a2ff52f755a87a5e37724d42fb161e49
Valderlan <valderlanjosr15@gmail.com>
2024-05-13 13:40:11 -0300

    adicionado conteudo

--- Backend/Express/Renderizando-HTML-dinamico/src/index.js
+++ Backend/Express/Renderizando-HTML-dinamico/src/index.js
@@ -4,8 +4,15 @@ const path = require('node:path');
 
 const app = express();
 
+const storedUsers = []
+
+//configuração do EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Configuração do body-parser
+app.use(express.urlencoded({ extended: true}))
+
 
 app.get('/', (req, res) => {
     const title = 'Renderizando HTML Dinâmico | EJS'
@@ -14,6 +21,23 @@ app.get('/', (req, res) => {
 
     res.render('index', { title, message })
 })
+
+app.get('/formulario', (req, res) => {
+    res.render('form')
+})
+
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    storedUsers.push({ username, password })
+
+    res.redirect('/usuarios')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.render('users', { users: storedUsers })
+} )
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Servidor foi iniciado'))
